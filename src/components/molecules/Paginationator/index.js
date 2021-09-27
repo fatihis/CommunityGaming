7184a919337528aef3ba92d108680a447b7aaf01
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-27 14:05:39 +0300

    Add Paginationator component

---
+++ src/components/molecules/Paginationator/index.js
@@ -0,0 +1,34 @@
+import { ActionButton } from "../ActionButton";
+
+export const Paginationator = ({
+  nomineePerPage,
+  totalNominees,
+  currentPage,
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalNominees / nomineePerPage); i++) {
+    pageNumbers.push(i);
+  }
+  return (
+    <div className="paginator flex mt-6 flex items-center justify-center">
+      {pageNumbers.map((number) =>
+        number.toString() == currentPage ? (
+          <ActionButton
+            onClick={() => (window.location.href = "/nominees/" + number)}
+            classes="text-white bg-gray-600 px-2 py-2 ml-2 font-bold"
+          >
+            {number}
+          </ActionButton>
+        ) : (
+          <ActionButton
+            onClick={() => (window.location.href = "/nominees/" + number)}
+            classes="text-gray-600 px-2 py-2 ml-2 font-bold"
+          >
+            {number}
+          </ActionButton>
+        )
+      )}
+    </div>
+  );
+};
